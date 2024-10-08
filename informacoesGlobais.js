d093e89497fb4285aa518e979ba4995f63ca15d1
Fernandakruk <128099736+Fernandakruk@users.noreply.github.com>
2024-10-08 09:36:34 -0300

    3 aula
    aaaaaa

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -12,7 +12,9 @@ visualizarInformacoesGlobais (){
     que aproximadamente ${dados.total_pessoas_concetadas}
     estão conectadas em alguma rede social e passem em médias 
     ${dados.tempo_medio} horas conectadas.`
-    console.log(parargafo)
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+    console.log(dados.tempo_medio)
 }
 visualizarInformacoesGlobais()
 {
@@ -20,3 +22,8 @@ visualizarInformacoesGlobais()
     "total_medio"; 2.38,
     "total_pessoas_mundo"; 7.888e9
 }
+paragrafo.innerHTML = `você sabia que o mundo tem 
+<span>${dados.total_pessoas_mundo}</span> de pessoas e 
+que aproxmadamente <span>${dados.total_pessoas_concetadas}</span>
+estão conectadas em alguma rede social e passam em media
+<span>${dados.tempo_medio}</span> horas conectadas.`
